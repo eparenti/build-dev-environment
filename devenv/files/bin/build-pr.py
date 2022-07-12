7c213d72d20c83df13f60cf7cc3bd986835e3bbd
Dallas Spohn <dspohn@redhat.com>
2022-07-12 14:34:02 -0500

    build-pr

--- devenv/files/bin/build-pr.py
+++ devenv/files/bin/build-pr.py
@@ -3,5 +3,5 @@ import os
 import time
 
 
-os.system('lab install -u --env test --version 0.0.1.dev0+pr.653 do374')
-
+# os.system('lab install -u --env test --version 0.0.1.dev0+pr.653 do374')
+os.system('lab install -u --env test --version 0.0.1.dev0 do467')
