221513637ece15be0c49d279fe0ecf91e75f7179
Dallas Spohn <dspohn@redhat.com>
2022-07-12 16:59:22 -0500

    build new PR

--- build-pr.py
+++ build-pr.py
@@ -4,4 +4,5 @@ import time
 
 
 # os.system('lab install -u --env test --version 0.0.1.dev0+pr.653 do374')
-os.system('lab install -u --env test --version 2.2.0.dev0 do467')
+# os.system('lab install -u --env test --version 2.2.0.dev0 do467')
+os.system('lab install -u --env test --version 2.2.0.dev0+pr.849 do467')
