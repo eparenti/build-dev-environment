07ec91428a7550ca20c2e1ea66aae43d51be6afd
Dallas Spohn <dallasspohn@gmail.com>
2022-08-30 23:27:46 -0500

    Create special.py
    Added a new script for testing labs.

---
+++ special.py
@@ -0,0 +1,43 @@
+import os
+import yaml
+
+chapters = ["ch2", "ch3", "ch4", "ch5", "ch6", "ch7", "ch8", "ch9"]
+
+ch2 = ["org-user", "org-team", "org-hub", "org-review"]
+ch3 = ["host-inventory", "host-credential", "host-review"]
+ch4 = ["provision-project", "provision-job", "provision-review"]
+ch5 = ["job-facts", "job-survey", "job-notification", "job-review"]
+ch6 = ["workflow-template", "workflow-approval", "workflow-review"]
+ch7 = ["advinventory-static", "advinventory-smart", "advinventory-review"]
+ch8 = ["code-collection"]  # ,'','']
+ch9 = ["admin-troubleshoot", "", ""]
+ch10 = ["", "", ""]
+ch11 = ["mesh-deploy-solve", "mesh-manage", ""]
+
+special = [
+    "host-inventory",
+    "host-review",
+    "job-facts",
+    "job-review",
+    "workflow-review",
+    "advinventory-smart",
+    "advinventory-review",
+    "code-api",
+    "code-review",
+    "admin-troubleshoot",
+]
+
+
+def start_lab():
+    for start in special:
+        print(start)
+        os.system("lab start " + start)
+
+def finish_lab():
+    for finish in special:
+        print(finish)
+        os.system("lab finish " + finish)
+#
+
+start_lab()
+finish_lab()
