03261fbe5300a6d23953ec50d34ae0f9b47ea73e
Dallas Spohn <dspohn@redhat.com>
2022-09-02 10:55:30 -0500

    template for automating labs with python.

--- run-all-labs.py
+++ run-all-labs.py
@@ -9,8 +9,8 @@ ch4 = ["provision-project", "provision-job", "provision-review"]
 ch5 = ["job-facts", "job-survey", "job-notification", "job-review"]
 ch6 = ["workflow-template", "workflow-approval", "workflow-review"]
 ch7 = ["advinventory-static", "advinventory-smart", "advinventory-review"]
-ch8 = ["code-collection"]  # ,'','']
-ch9 = ["admin-troubleshoot", "", ""]
+ch8 = ["code-collection", "code-webhooks", "code-api", "code-review"]  # ,'','']
+ch9 = ["admin-troubleshoot", "admin-recovery"]
 ch10 = ["", "", ""]
 ch11 = ["mesh-deploy-solve", "mesh-manage", ""]
 
@@ -18,34 +18,47 @@ ch11 = ["mesh-deploy-solve", "mesh-manage", ""]
 for c2 in ch2:
     print(c2)
     os.system("lab start " + c2)
+    os.system("lab finish " + c2)
 
 for c3 in ch3:
     print(c3)
     os.system("lab start " + c3)
+    os.system("lab finish " + c3)
 
 for c4 in ch4:
     print(c4)
     os.system("lab start " + c4)
+    os.system("lab finish " + c4)
 
 for c5 in ch5:
     print(c5)
     os.system("lab start " + c5)
+    os.system("lab finish " + c5)
 
 for c6 in ch6:
     print(c6)
     os.system("lab start " + c6)
+    os.system("lab finish " + c6)
 
 for c7 in ch7:
     print(c7)
     os.system("lab start " + c7)
+    os.system("lab finish " + c7)
 
 for c8 in ch8:
     print(c8)
     os.system("lab start " + c8)
+    os.system("lab finish " + c8)
 
 for c9 in ch9:
     print(c9)
-    os.system("lab-start " + c9)
+    os.system("lab start " + c9)
+    os.system("lab finish " + c9)
+
+for c11 in ch11:
+    print(c9)
+    os.system("lab start " + c11)
+    os.system("lab finish " + c11)
 
 
 # with open("lab-scripts.yml", 'r') as stream:
