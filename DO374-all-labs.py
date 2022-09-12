0f1b08a29e4e85c956b53d10bdc804aac4883b6c
Dallas Spohn <dspohn@redhat.com>
2022-09-12 09:56:57 -0500

    Edit run all scripts.

--- DO374-all-labs.py
+++ DO374-all-labs.py
@@ -14,53 +14,60 @@ ch8 = ['update-delegation', 'update-parallelism', 'update-management', 'update-r
 ch9 = ['create-writing', '', 'builder-custom', 'builder-validate', 'builder-use', 'builder-review']
 ch10 = ['review-cr1', 'review-cr2', 'review-cr3']
 
+lab_name = 'This lab is '
+
+for c1 in ch1:
+    print(lab_name, c1)
+    os.system("lab start " + c1)
+    os.system("lab finish " + c1)
+
 for c2 in ch2:
-    print(c2)
+    print(lab_name, c2)
     os.system("lab start " + c2)
     os.system("lab finish " + c2)
 
 for c3 in ch3:
-    print(c3)
+    print(lab_name, c3)
     os.system("lab start " + c3)
     os.system("lab finish " + c3)
 
 for c4 in ch4:
-    print(c4)
+    print(lab_name, c4)
     os.system("lab start " + c4)
     os.system("lab finish " + c4)
 
 for c5 in ch5:
-    print(c5)
+    print(lab_name, c5)
     os.system("lab start " + c5)
     os.system("lab finish " + c5)
 
 for c6 in ch6:
-    print(c6)
+    print(lab_name, c6)
     os.system("lab start " + c6)
     os.system("lab finish " + c6)
 
 for c7 in ch7:
-    print(c7)
+    print(lab_name, c7)
     os.system("lab start " + c7)
     os.system("lab finish " + c7)
 
 for c8 in ch8:
-    print(c8)
+    print(lab_name, c8)
     os.system("lab start " + c8)
     os.system("lab finish " + c8)
 
 for c9 in ch9:
-    print(c9)
+    print(lab_name, c9)
     os.system("lab start " + c9)
     os.system("lab finish " + c9)
 
 for c11 in ch11:
-    print(c11)
+    print(lab_name, c11)
     os.system("lab start " + c11)
     os.system("lab finish " + c11)
 
 for c12 in ch12:
-    print(c12)
+    print(lab_name, c12)
     os.system("lab start " + c12)
     os.system("lab finish " + c12)
 
